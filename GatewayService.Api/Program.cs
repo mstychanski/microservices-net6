3383372dacfe7301a389fbb402c370ce8ce441e4
Mateusz Stychanski <K99000178@kimballelectronics.com>
2022-05-20 09:35:29 +0200

    YARP GatewayApi added.

---
+++ GatewayService.Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+var app = builder.Build();
+
+
+//dotnet add package Yarp.ReverseProxy
+app.MapGet("/", () => "Hello Gateway Api!!");
+
+
+app.MapReverseProxy();
+s
+app.Run();
